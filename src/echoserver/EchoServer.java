e84f9dc3e4af88fda292ad5f1b0871e6dd5707ec
walte774 <walte774@morris.umn.edu>
2019-10-10 11:39:02 -0500

    Write EchoClient and EchoServer
    We use the provided DateClient and DateServer as a basis.
    None of this has been tested yet.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,33 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+	public static final int portNumber = 6013;
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket sock = new ServerSocket(portNumber);
+			while(true) {
+				Socket client = sock.accept();
+				System.out.println("Made a new friend!");
+
+				OutputStream output = client.getOutputStream();
+				InputStream input = client.getInputStream();
+
+				int b;
+				while ((b = input.read()) != -1) {
+					output.write((byte)b);
+				}
+				
+				output.close();
+				input.close();
+				client.close();
+				
+			}
+		} catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
+		}
+	}
 }
